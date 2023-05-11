b0b1daaa12926b124eb5729a0ab107e0461ebc19
Marina Flores <mlbtf@discente.ifpe.edu.br>
2023-05-11 17:20:27 -0300

    First Commit

---
+++ src/test/java/projetoweb/ExampleTest.java
@@ -0,0 +1,21 @@
+package projetoweb;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
+public class ExampleTest {
+    
+    @Test
+    public void testGoogleTitle() {
+        System.setProperty("webdriver.chrome.driver", "C://driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8080/projetoweb");
+        String pageTitle = driver.getTitle();
+        System.out.println(pageTitle);
+        Assert.assertEquals("Hello World!", pageTitle);
+        driver.quit();
+    }
+}
